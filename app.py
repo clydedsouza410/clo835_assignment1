0170d7fbdca153492cd9cd5cee02190cd39e48b7
clydedsouza410 <clydedsouza410@gmail.com>
2026-01-31 12:34:25 -0500

    fix app.py

--- app.py
+++ app.py
@@ -16,15 +16,14 @@ DBPORT = int(os.environ.get("DBPORT") or 3306)
 
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
-    host= DBHOST,
+    host=DBHOST,
     port=DBPORT,
-    user= DBUSER,
-    password= DBPWD, 
-    db= DATABASE
-    
+    user=DBUSER,
+    password=DBPWD,
+    db=DATABASE
 )
-output = {}
-table = 'employee';
+
+table = 'employee'
 
 # Define the supported color codes
 color_codes = {
@@ -37,7 +36,6 @@ color_codes = {
     "lime": "#C1FF9C",
 }
 
-
 # Create a string of supported colors
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
@@ -49,10 +47,12 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
 def home():
     return render_template('addemp.html', color=color_codes[COLOR])
 
-@app.route("/about", methods=['GET','POST'])
+
+@app.route("/about", methods=['GET', 'POST'])
 def about():
     return render_template('about.html', color=color_codes[COLOR])
-    
+
+
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
@@ -61,57 +61,84 @@ def AddEmp():
     primary_skill = request.form['primary_skill']
     location = request.form['location']
 
-  
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
-        
-        cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-
     finally:
         cursor.close()
 
     print("all modification done...")
     return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
 
+
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
     return render_template("getemp.html", color=color_codes[COLOR])
 
 
-@app.route("/fetchdata", methods=['GET','POST'])
+@app.route("/fetchdata", methods=['GET', 'POST'])
 def FetchData():
     emp_id = request.form['emp_id']
 
-    output = {}
-    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
+    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location FROM employee WHERE emp_id=%s"
     cursor = db_conn.cursor()
 
     try:
-        cursor.execute(select_sql,(emp_id))
+        # IMPORTANT: (emp_id,) must be a tuple
+        cursor.execute(select_sql, (emp_id,))
         result = cursor.fetchone()
-        
-        # Add No Employee found form
+
+        # If no record found
+        if result is None:
+            return render_template(
+                "getempoutput.html",
+                id="N/A",
+                fname="No",
+                lname="Employee Found",
+                interest="N/A",
+                location="N/A",
+                color=color_codes[COLOR]
+            )
+
+        output = {}
         output["emp_id"] = result[0]
         output["first_name"] = result[1]
         output["last_name"] = result[2]
         output["primary_skills"] = result[3]
         output["location"] = result[4]
-        
+
     except Exception as e:
         print(e)
+        return render_template(
+            "getempoutput.html",
+            id="Error",
+            fname="Error",
+            lname="Occurred",
+            interest="Check logs",
+            location="N/A",
+            color=color_codes[COLOR]
+        )
 
     finally:
         cursor.close()
 
-    return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+    return render_template(
+        "getempoutput.html",
+        id=output["emp_id"],
+        fname=output["first_name"],
+        lname=output["last_name"],
+        interest=output["primary_skills"],
+        location=output["location"],
+        color=color_codes[COLOR]
+    )
+
 
 if __name__ == '__main__':
-    
+
     # Check for Command Line Parameters for color
     parser = argparse.ArgumentParser()
     parser.add_argument('--color', required=False)
@@ -121,7 +148,8 @@ if __name__ == '__main__':
         print("Color from command line argument =" + args.color)
         COLOR = args.color
         if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
+            print("A color was set through environment variable -" + COLOR_FROM_ENV +
+                  ". However, color from command line argument takes precendence.")
     elif COLOR_FROM_ENV:
         print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
         COLOR = COLOR_FROM_ENV
@@ -133,4 +161,4 @@ if __name__ == '__main__':
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
 
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
